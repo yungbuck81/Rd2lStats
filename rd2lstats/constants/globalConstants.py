ee73d74c3cb7fb0f218d34c20b6bf63e0e17fe29
Sanjeev Rajasekaran <sanjeev95@rocketmail.com>
2022-01-12 23:03:39 -0800

    Initial commit

---
+++ rd2lstats/constants/globalConstants.py
@@ -0,0 +1,61 @@
+
+# Contains the current week match ID's to parse stats for
+match_ids = ["6062783117", "6062831827", "6062830154", "6062789342", "6062779562", "6062776919", "6062837222", "6062820546", "6062800178", "6062778380", "6062832242", "6062780059", "6062776994", "6062831560", "6062851356", "6062798323", "6062851728", "6062777961", "6062839966", "6062820792"]
+
+# Admin discord ID
+sharingan_id = 252382118510854144
+
+# Used for fetching Hero icon images in embeds
+steam_cdn = "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/"
+
+# Dotabuff base URL
+dotabuff_url = "https://www.dotabuff.com/matches/"
+
+# Opendota API URL
+opendota_api_players_url = "https://api.opendota.com/api/players/"
+opendota_api_matches_url = "https://api.opendota.com/api/matches/"
+
+# Stats multipliers, these are slightly modified from Dota's fantasy point calculation
+fantasy_kill_multiplier = 0.5
+fantasy_death_multiplier = -0.5
+fantasy_assist_multiplier = 0.15
+fantasy_lasthit_multiplier = 0.005
+fantasy_gpm_multiplier = 0.005
+fantasy_ts_multiplier = 1
+fantasy_roshan_multiplier = 1
+fantasy_wards_planted = 0.3
+fantasy_wards_dewarded = 0.4
+fantasy_camps_stacked = 0.4
+fantasy_runes_grabbed = 0.25
+fantasy_first_blood = 3
+fantasy_stuns_multiplier = 0.05
+
+# Pos 1 constants
+pos1directory = "../pos1stats/"
+pos1gpmfile = "pos1gpmLeaderboard.csv"
+pos1kdafile = "pos1kdaLeaderboard.csv"
+pos1fantasyfile = "pos1fantasyLeaderboard.csv"
+
+# Pos 2 constants
+pos2directory = "../pos2stats/"
+pos2gpmfile = "pos2gpmLeaderboard.csv"
+pos2kdafile = "pos2kdaLeaderboard.csv"
+pos2fantasyfile = "pos2fantasyLeaderboard.csv"
+
+# Pos 3 constants
+pos3directory = "../pos3stats/"
+pos3gpmfile = "pos3gpmLeaderboard.csv"
+pos3kdafile = "pos3kdaLeaderboard.csv"
+pos3fantasyfile = "pos3fantasyLeaderboard.csv"
+
+# Pos 4 constants
+pos4directory = "../pos4stats/"
+pos4gpmfile = "pos4gpmLeaderboard.csv"
+pos4kdafile = "pos4kdaLeaderboard.csv"
+pos4fantasyfile = "pos4fantasyLeaderboard.csv"
+
+# Pos 5 constants
+pos5directory = "../pos5stats/"
+pos5gpmfile = "pos5gpmLeaderboard.csv"
+pos5kdafile = "pos5kdaLeaderboard.csv"
+pos5fantasyfile = "pos5fantasyLeaderboard.csv"
